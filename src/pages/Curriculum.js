04a884bbed9c245fdaf04f898a46c9eecac78c69
JuanmaPauso <juanmapauso@gmail.com>
2021-10-08 10:31:34 +0200

    Update Media Queries

--- src/pages/Curriculum.js
+++ src/pages/Curriculum.js
@@ -1,28 +1,56 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Curriculum = () => {
+  const isDesktopOrLaptop = useMediaQuery({ query: "(min-width: 721px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 720px)" });
+
   return (
     <div className="page">
-      <section>
-        <div className="presentacion">
-          <div className="foto"></div>
-          <div>
-            <p>
-              Hola, me llamo Juanma Pérez y soy diseñador gráfico. Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Nobis obcaecati illum
-              exercitationem delectus repellat ab excepturi architecto quaerat
-              ut ad, adipisci harum inventore vel dolore, perspiciatis
-              necessitatibus. Facilis, soluta. Error?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              obcaecati illum exercitationem delectus repellat ab excepturi
-              architecto quaerat ut ad, adipisci harum inventore vel dolore,
-              perspiciatis necessitatibus. Facilis, soluta. Error?
-            </p>
+      {isDesktopOrLaptop && (
+        <section>
+          <div className="presentacion">
+            <div className="foto"></div>
+            <div>
+              <p>
+                Hola, me llamo Juanma Pérez y soy diseñador gráfico. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Nobis obcaecati
+                illum exercitationem delectus repellat ab excepturi architecto
+                quaerat ut ad, adipisci harum inventore vel dolore, perspiciatis
+                necessitatibus. Facilis, soluta. Error?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                obcaecati illum exercitationem delectus repellat ab excepturi
+                architecto quaerat ut ad, adipisci harum inventore vel dolore,
+                perspiciatis necessitatibus. Facilis, soluta. Error?
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
+      {isTabletOrMobile && (
+        <section>
+          <div className="presentacion">
+            <div className="foto"></div>
+            <div>
+              <p>
+                Hola, me llamo Juanma Pérez y soy diseñador gráfico. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Nobis obcaecati
+                illum exercitationem delectus repellat ab excepturi architecto
+                quaerat ut ad, adipisci harum inventore vel dolore, perspiciatis
+                necessitatibus. Facilis, soluta. Error?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                obcaecati illum exercitationem delectus repellat ab excepturi
+                architecto quaerat ut ad, adipisci harum inventore vel dolore,
+                perspiciatis necessitatibus. Facilis, soluta. Error?
+              </p>
+            </div>
+          </div>
+        </section>
+      )}
     </div>
   );
 };
