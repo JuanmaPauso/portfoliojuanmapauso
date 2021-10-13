6ca6d24adac1acd92afc6287a32475b95e89c4a1
JuanmaPauso <juanmapauso@gmail.com>
2021-10-13 11:11:21 +0200

    Update Curriculum Text

--- src/pages/Curriculum.js
+++ src/pages/Curriculum.js
@@ -8,20 +8,22 @@ const Curriculum = () => {
           <div className="foto"></div>
           <div>
             <p className="parrafo">
-              Hola, me llamo Juanma Pérez, soy Diseñador UX/UI y Frontend
-              Developer. 
+              Hola, me llamo Juanma Pérez, soy un diseñador multidisciplinar y
+              recientemente formado como desarrollador web, actualmente vivo en
+              Valencia, apasionado de las nuevas tecnologías y enamorado de los
+              Cómic desde la infancia.
             </p>
             <p className="parrafo">
-              En los últimos 8 años he estado detrás del desarrollo
-              UX/UI de proyectos web, app y juegos multiplataformas en diversas
-              empresas. Como desarrollador web tengo conocimientos en Html5,
-              CSS3, SASS, Javascript, jquery, node.js, express.js y sequelize.
-              Además de usar Git junto a Github como control de versiones.
+              En mis 8 años de experiencia, he tenido la suerte de trabajar en
+              varias empresas con modelos de negocios diversos desde la
+              gamificación hasta los entrenamientos online, desarrollando mis
+              diversas habilidades gráficas (diseño UX/UI, ilustración, motion
+              grapher, 3D…).
             </p>
             <p className="parrafo">
-              Mis habilidades gráficas son el diseño online y offline, la edición de
-              video (motion graphics), la ilustración, la fotografía digital y
-              el modelado 3D (Cinema 4D - Vray).
+              En los últimos años me he ido especializando en el diseño UX/UI y
+              en la programación web (React, Node.js, MongoDB…) para poder
+              desarrollar un proyecto web desde cero.
             </p>
           </div>
         </div>
