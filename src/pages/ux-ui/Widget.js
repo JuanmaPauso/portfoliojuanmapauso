ff0889db9936d973a5d6badb9d3f89bb02e66099
JuanmaPauso <juanmapauso@gmail.com>
2022-03-27 18:36:14 +0200

    Update Widget

--- src/pages/ux-ui/Widget.js
+++ src/pages/ux-ui/Widget.js
@@ -7,61 +7,47 @@ const Widget = () => {
       <div className="trabajo">
         <img
           className="portada-trabajo"
-          src={`${process.env.PUBLIC_URL}/images/games-cover.jpg`}
-          alt="portada-juegos"
+          src={`${process.env.PUBLIC_URL}/images/widget-cover.jpg`}
+          alt="portada-widget"
         />
 
         <h1 className="titulo-trabajo">WIDGET DE GAMIFICACIÓN</h1>
 
         <p className="parrafo">
-          Hooptap era una empresa que se dedicaba a la gamificiación, ofrecía
-          más de 50 mecánicas modificables a través de su plataforma.
+        Hooptap era una empresa que se dedicaba a la Gamificación, anteriormente implementaba está creando entornos web totalmente nuevos, pero los clientes demandaban un producto que se adaptara a su web sin tener que crear nuevas direcciones, entonces nació el concepto de un widget que se adjuntara como una capa por encima del sitio del cliente.
         </p>
         <p className="parrafo">
-          El problema era que los juegos se fueron desarrollando sobre la
-          marcha por diversos diseñadores, cada uno con un diseño único,
-          complicando exponencialmente el trabajo para su personalización, al
-          tener que adaptar las imágenes a diversos formatos y tamaños. Tal
-          era la complejidad que pocos clientes modificaban los juegos y en la
-          mayoría de los casos nos encargaban a nosotros realizar los cambios.
+        El problema del primer prototipo es que era poco práctico para adaptarlo a diversos entornos, aparte de ser muy complejo en funcionalidades. El objetivo era crear un widget personalizable, que se pudiera adaptar al diseño del cliente aparte de simplificar su funcionamiento.
+
+        </p>
+
+        <p className="parrafo">
+        Se eliminaron funcionalidades en busca de un producto totalmente enfocado a la Gamificación. Los diversos elementos, como cabeceras, iconos etc , se modificaron para dar mayores posibilidades de configuración a los clientes.
+
+        </p>
+        <p className="parrafo">
+        El diseño fue realizado en Sketch, los iconos en Illustrator y los mockups de ejemplos de adaptaciones con Photoshop.
+
         </p>
 
         <img
           className="imagen-trabajo"
-          src={`${process.env.PUBLIC_URL}/images/games-old-images.png`}
+          src={`${process.env.PUBLIC_URL}/images/Web-coffe-2.png`}
           alt="portada-juegos"
         />
-        <div className="pie-foto">Diseños de juegos antiguos</div>
+        <div className="pie-foto">Mockup de cadena de cafeterias</div>
 
         <p className="parrafo">
-          La solución fue un rediseño de todos los juegos de la plataforma de
-          gamificación, mejorando la estructura y aunando los formatos de las
-          imágenes para facilitar su modificación.
-        </p>
-        <p className="parrafo">
-          Se estudio la estructura de todos los juegos buscando los elementos
-          en común, que fueron el scoreboard y el fondo, a partir de ahí se
-          determino un mismo tamaño y formato para todos los elementos
-          visuales.
+        El resultado fue una herramienta más personalizable, util y fácil de manejar para el futuro cliente.
         </p>
 
         <img
           className="imagen-trabajo"
-          src={`${process.env.PUBLIC_URL}/images/games-new-images.png`}
+          src={`${process.env.PUBLIC_URL}/images/Web-airlines-2.png`}
           alt="portada-juegos"
         />
-        <div className="pie-foto">Rediseño de los juegos</div>
-
-        <p className="parrafo">
-          El diseño del Scoreboard tenia que ser una estructura que albergara
-          todo tipo de información desde el tiempo, la puntuación, etc. Se
-          estructuro de forma modular, cambiando la información según la
-          mecánica lo requería.
-        </p>
-        <p className="parrafo">
-          El resultado final fue un ahorro de tiempo y recursos, facilitando
-          el uso para usuarios que no fueran diseñadores.
-        </p>
+        <div className="pie-foto">Mockup de Aerolinea</div>
+        
       </div>
     </section>
   </div>
