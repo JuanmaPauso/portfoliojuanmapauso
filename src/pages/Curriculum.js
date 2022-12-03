b7211510b64c3bb4d5a1fa773b1251fe950181ef
JuanmaPauso <juanmapauso@gmail.com>
2022-12-03 11:57:40 +0100

    Update Datos & Curriculum

--- src/pages/Curriculum.js
+++ src/pages/Curriculum.js
@@ -8,20 +8,20 @@ const Curriculum = () => {
           <div className="foto"></div>
           <div className="biografia">
             <h2>Juanma Pérez</h2>
-            <h3>Diseñador UX/UI y desarrollador web</h3>
+            <h3>Diseñador Multidisciplinar y desarrollador web</h3>
             <p className="parrafo">
               Actualmente vivo en Valencia, apasionado de las nuevas tecnologías
               y enamorado de los Cómic desde la infancia.
             </p>
             <p className="parrafo">
-              En mis 8 años de experiencia, he tenido la suerte de trabajar en
+              En mis más de 9 años de experiencia, he tenido la suerte de trabajar en
               varias empresas con modelos de negocios diversos desde la
               gamificación hasta los entrenamientos online, desarrollando mis
-              diversas habilidades gráficas (diseño UX/UI, ilustración, motion
+              diversas habilidades gráficas (diseño UI, ilustración, motion
               grapher, 3D…).
             </p>
             <p className="parrafo">
-              En los últimos años me he ido especializando en el diseño UX/UI y
+              En los últimos años me he ido especializando en el diseño UI y
               en la programación web (React, Node.js, MongoDB…) para poder
               liderar un proyecto web desde cero.
             </p>
